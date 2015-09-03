3e475c0ce36ebe665e3adfb2bde84682ee6f9255
Joao Neto <joao.neto@fieb.org.br>
2015-09-03 15:03:43 -0300

    No backward compatibility. Draf version

---
+++ tasks/UsblAUV.cpp
@@ -0,0 +1,54 @@
+/* Generated from orogen/lib/orogen/templates/tasks/Task.cpp */
+
+#include "UsblAUV.hpp"
+
+using namespace usbl_evologics;
+
+UsblAUV::UsblAUV(std::string const& name)
+    : UsblAUVBase(name)
+{
+}
+
+UsblAUV::UsblAUV(std::string const& name, RTT::ExecutionEngine* engine)
+    : UsblAUVBase(name, engine)
+{
+}
+
+UsblAUV::~UsblAUV()
+{
+}
+
+
+
+/// The following lines are template definitions for the various state machine
+// hooks defined by Orocos::RTT. See UsblAUV.hpp for more detailed
+// documentation about them.
+
+bool UsblAUV::configureHook()
+{
+    if (! UsblAUVBase::configureHook())
+        return false;
+    return true;
+}
+bool UsblAUV::startHook()
+{
+    if (! UsblAUVBase::startHook())
+        return false;
+    return true;
+}
+void UsblAUV::updateHook()
+{
+    UsblAUVBase::updateHook();
+}
+void UsblAUV::errorHook()
+{
+    UsblAUVBase::errorHook();
+}
+void UsblAUV::stopHook()
+{
+    UsblAUVBase::stopHook();
+}
+void UsblAUV::cleanupHook()
+{
+    UsblAUVBase::cleanupHook();
+}
